42d8dfd5ed5e4c3735d9bab70ae8bbe55ffdac82
Khairul Rijal <khairulrijl@gmail.com>
2020-01-12 10:41:21 +0700

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Khairul Rijal on 12/01/20.
+//  Copyright © 2020 Khairul Rijal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
